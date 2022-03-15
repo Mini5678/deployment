7b0c8b1532dc8e9b3733d82d42825ae22f58d910
Mini5678 <101258625+Mini5678@users.noreply.github.com>
2022-03-15 17:02:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRun2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's learn anything");
+        }
+    }
+}
